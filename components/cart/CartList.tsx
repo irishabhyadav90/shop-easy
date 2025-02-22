78232d6f0efba731cd8bd85c4b7c66dff3b281bf
Rishabh Yadav <39909810+irishabhyadav90@users.noreply.github.com>
2025-02-23 03:35:13 +0530

    feat: Add Cart screen with dynamic header (#7)

---
+++ components/cart/CartList.tsx
@@ -0,0 +1,27 @@
+import { Text, View, Image } from 'react-native';
+import { styles } from './styles';
+import { CartItem } from '@/store/cartStore';
+
+export const CartList = ({ items }: { items: CartItem[] }) => {
+
+    return (
+        <View style={styles.container}>
+            {items.map((item: CartItem) => (
+                <View key={item.product.id} style={styles.cartItem}>
+                    <Image
+                        source={{ uri: item.product.images.featured_image }}
+                        style={styles.image}
+                    />
+                    <View style={styles.details}>
+                        <Text style={styles.title} numberOfLines={2}>
+                            {item.product.title}
+                        </Text>
+                        <Text style={styles.price}>
+                            {item.product.sale.currency} {item.product.sale.regular_price}
+                        </Text>
+                    </View>
+                </View>
+            ))}
+        </View>
+    );
+};
