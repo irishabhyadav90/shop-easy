23332d28ed7614f5c696294fff779043447d287a
Rishabh Yadav <39909810+irishabhyadav90@users.noreply.github.com>
2025-02-23 13:43:34 +0530

    chore: Add android build config (#11)

--- app.config.js
+++ app.config.js
@@ -1,9 +1,17 @@
 import 'dotenv/config';
 
+// Todo - I'm sharing the projectId just for now, The configuration should be app/organization level
+
 export default {
     expo: {
         extra: {
             BASE_URL: process.env.BASE_URL,
+            "eas": {
+                "projectId": "e4137959-dd4e-40d1-bc11-09042c1830b5"
+            }
         },
+        android: {
+            package: "com.shopeasy.com" // Replace this with your actual package name
+        }
     }
 };
